53f6321199b222d7d4254850c72b34c1664cf226
mohammn <mvapdg@gmail.com>
2020-08-31 14:09:04 +0700

    menampilkan user

--- application/controllers/user_control.php
+++ application/controllers/user_control.php
@@ -1,12 +1,22 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class user_control extends CI_Controller {
+class user_control extends CI_Controller
+{
 
 
 	public function index()
 	{
-        //echo  'hello panda';
-        $this->template->load('template','user');
+		//echo  'hello panda';
+		$this->template->load('template', 'user');
+		$this->load->model('db_model');
+		$this->load->library('form_validation');
+	}
+
+	public function get_data()
+	{
+		$target = $this->input->post("target");
+		$data = $this->db_model->get_all($target)->result();
+		echo json_encode($data);
 	}
 }

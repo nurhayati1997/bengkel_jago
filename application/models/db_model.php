815c49c65d1e1053a77abacd50d5263b6c4f6948
mohammn <mvapdg@gmail.com>
2020-09-01 00:38:49 +0700

    menambah model db_model.php

---
+++ application/models/db_model.php
@@ -0,0 +1,44 @@
+<?php
+class Db_model extends CI_Model
+{
+    public function insert($tabel, $data)
+    {
+        $this->db->insert($tabel, $data);
+    }
+
+    public function get_where($table, $where)
+    {
+        return $this->db->get_where($table, $where);
+    }
+
+    public function get_query($query)
+    {
+        return $this->db->query($query);
+    }
+
+    public function get_all($tabel)
+    {
+        return $this->db->get($tabel);
+    }
+
+    public function update($tabel, $data, $where)
+    {
+        $this->db->update($tabel, $data, $where);
+    }
+
+    public function delete($tabel, $where)
+    {
+        $this->db->delete($tabel, $where);
+    }
+    function getDataAgregat($tgl, $col)
+    {
+        $query = $this->db->where($col, $tgl)->get('vwpasien');
+
+        return $query->result();
+    }
+    function getDataStatus($col, $table)
+    {
+        $query = $this->db->select($col)->get($table)->result();
+        return $query;
+    }
+}

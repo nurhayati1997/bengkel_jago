b05a8b70efd0b8abb61089bdc5306320e35fb6ad
mohammn <mvapdg@gmail.com>
2020-09-01 23:27:09 +0700

    menampilkan data client

--- application/controllers/client_control.php
+++ application/controllers/client_control.php
@@ -1,12 +1,25 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class client_control extends CI_Controller {
-
+class client_control extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('db_model');
+		$this->load->library('form_validation');
+	}
 
 	public function index()
 	{
-        //echo  'hello panda';
-        $this->template->load('template','client');
+		//echo  'hello panda';
+		$this->template->load('template', 'client');
+	}
+
+	public function get_data()
+	{
+		$tabel = $this->input->post("target");
+		$data = $this->db_model->get_all($tabel)->result_array();
+		echo json_encode($data);
 	}
 }

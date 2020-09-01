ee17f80fd357c1406a3d244618920ed2e165e63f
mohammn <mvapdg@gmail.com>
2020-09-01 11:26:29 +0700

    tampilan login dan persiapan controller

--- application/controllers/login_control.php
+++ application/controllers/login_control.php
@@ -1,12 +1,94 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class login_control extends CI_Controller {
+class login_control extends CI_Controller
+{
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('db_model');
+		$this->load->library('form_validation');
+	}
 
 	public function index()
 	{
-        //echo  'hello panda';
-        $this->load->view('login');
+		// if ($this->session->userdata("kode")) {
+		// 	redirect("dashboard");
+		// }
+		$this->form_validation->set_rules('password', 'Password', 'required|trim');
+		if ($this->form_validation->run() == false) {
+			$data['title'] = "Login";
+			$this->load->view('login', $data);
+		} else {
+			$this->_login();
+		}
+	}
+
+	public function ambilData()
+	{
+		$data = $this->db_model->get_all($this->input->get('target'));
+		echo json_encode($data->result());
+	}
+
+	private function _login()
+	{
+		$data['title'] = "Login";
+		$idKecamatan = $this->input->post("idKecamatan");
+		$password = $this->input->post("password");
+		$user = $this->db_model->get_where("tbl_Kecamatan", ["KODE_KECAMATAN" => $idKecamatan])->row_array();
+
+		if ($user) {
+			if (password_verify($password, $user['password'])) {
+				$data = [
+					'kode' => $user['KODE_KECAMATAN']
+				];
+				$this->session->set_userdata($data);
+
+				redirect('dashboard');
+			} else {
+				$this->session->set_flashdata('gagal_login', 'Maaf, Password anda salah :(');
+				redirect('Auth', $data);
+			}
+		} else {
+			$this->session->set_flashdata('gagal_login', 'Maaf, Kecamatan tersebut belum terdaftar :(');
+			redirect('Auth', $data);
+		}
+	}
+
+	public function ubahPassword()
+	{
+		$this->form_validation->set_rules('passBaru', 'Password', 'required|trim|min_length[5]|matches[passVerif]');
+		$this->form_validation->set_rules('passVerif', 'Verifikasi Password', 'required|trim|matches[passBaru]');
+
+		$data['title'] = "Ubah password";
+		if ($this->form_validation->run()) {
+			$password = password_hash($this->$this->input->post("passBaru"), PASSWORD_DEFAULT);
+			$email = $this->session->userdata("reset_email");
+
+			$data = ["password" => $password];
+			$this->database_model->update("user", $data, ["email" => $email]);
+
+			$this->session->unset_userdata('reset_email');
+
+			$this->session->set_flashdata('berhasil_logout', 'Password berhasil diubah. Silahkan Log in :)');
+			redirect('auth');
+		} else {
+			$this->render_auth("auth/resetPassword", $data);
+		}
+	}
+
+	public function logout()
+	{
+		$this->session->unset_userdata('kode');
+		$this->session->set_flashdata('berhasil_logout', 'Anda telah berhasil log out. Terimkasih :)');
+		$data['title'] = "Login";
+		redirect('Auth', $data);
+	}
+
+	public function blocked()
+	{
+		$data['title'] = "403 Forbidden";
+		$this->render_auth("auth/blocked", $data);
 	}
 }
--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,11 +8,12 @@
     <title>Karisma Motor</title>
 
     <!-- Font Icon -->
-    <link rel="stylesheet" href="<?=base_url()?>asset_login/fonts/material-icon/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>asset_login/fonts/material-icon/css/material-design-iconic-font.min.css">
 
     <!-- Main css -->
-    <link rel="stylesheet" href="<?=base_url()?>asset_login/css/style.css">
+    <link rel="stylesheet" href="<?= base_url() ?>asset_login/css/style.css">
 </head>
+
 <body>
 
     <div class="main">
@@ -20,7 +22,7 @@
             <div class="container">
                 <div class="signin-content">
                     <div class="signin-image">
-                        <figure><img src="<?=base_url()?>asset_login/images/signin-image.jpg" alt="sing up image"></figure>
+                        <figure><img src="<?= base_url() ?>asset_login/images/signin-image.jpg" alt="sing up image"></figure>
                         <a href="#" class="signup-image-link"> </a>
                     </div>
 
@@ -29,18 +31,20 @@
                         <form method="POST" class="register-form" id="login-form">
                             <div class="form-group">
                                 <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="your_name" id="your_name" placeholder="Your Name"/>
+                                <select name="nama" class="form-control" id="nama">
+                                </select>
+                                <small class="text-danger"><?= form_error('nama'); ?></small>
                             </div>
                             <div class="form-group">
                                 <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="your_pass" id="your_pass" placeholder="Password"/>
+                                <input type="password" name="your_pass" id="your_pass" placeholder="Password" />
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
                                 <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
                             </div>
                             <div class="form-group form-button">
-                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
+                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in" />
                             </div>
                         </form>
                         <div class="social-login">
@@ -59,7 +63,38 @@
     </div>
 
     <!-- JS -->
-    <script src="<?=base_url()?>asset_login/vendor/jquery/jquery.min.js"></script>
-    <script src="<?=base_url()?>asset_login/js/main.js"></script>
+    <script src="<?= base_url() ?>assets/js/core/jquery.3.2.1.min.js"></script>
+    <script src="<?= base_url() ?>asset_login/js/main.js"></script>
+    <script>
+        get_list_desa();
+
+        function get_list_desa() {
+            $.ajax({
+                type: 'get',
+                data: 'target=tbl_pengguna',
+                url: '<?= base_url() ?>login_control/ambilData',
+                dataType: 'json',
+                success: function(data) {
+                    var baris = '<option value="0">- Pilih Pengguna -</option>';
+                    var nama = '';
+                    for (var i = 0; i < data.length; i++) {
+                        baris += '<option value = "' + data[i].id_pengguna + '"'
+                        if (data[i].id_pengguna == <?php if (set_value("nama")) {
+                                                        echo "'" . set_value("nama") . "'";
+                                                    } else {
+                                                        echo "0";
+                                                    } ?>) {
+                            baris += "selected"
+                        }
+                        nama = data[i].nama
+                        baris += ' > ' + nama + ' </option>'
+                    }
+
+                    $("#nama").html(baris);
+                }
+            });
+        }
+    </script>
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
 </html>

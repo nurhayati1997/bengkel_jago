865cdd776d17e22f6187714d4d47bd2d26735c77
mohammn <mvapdg@gmail.com>
2020-09-03 23:10:44 +0700

    menampilkan fungsi laporan keuntungan. (belum selesai ada yang undefined)

--- application/controllers/keuntungan_control.php
+++ application/controllers/keuntungan_control.php
@@ -1,12 +1,27 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class keuntungan_control extends CI_Controller {
-
+class keuntungan_control extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('db_model');
+		$this->load->library('form_validation');
+	}
 
 	public function index()
 	{
-        //echo  'hello panda';
-        $this->template->load('template','keuntungan');
+		//echo  'hello panda';
+		$this->template->load('template', 'keuntungan');
+	}
+
+	public function get_data()
+	{
+		$target = $this->input->post('target');
+		$tanggalMulai = $this->input->post('tanggalMulai');
+		$tanggalSelesai = $this->input->post('tanggalSelesai');
+		$data = $this->db_model->get_where("tbl_penjualan", ['tgl_penjualan >=' => $tanggalMulai, 'tgl_penjualan <=' => $tanggalSelesai])->result_array();
+		echo json_encode($data);
 	}
 }
--- application/views/keuntungan.php
+++ application/views/keuntungan.php
@@ -4,7 +4,7 @@
 						<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
 							<div>
 								<h2 class="text-white pb-2 fw-bold">Laporan Keuntungan</h2>
-						
+
 							</div>
 						</div>
 					</div>
@@ -29,25 +29,25 @@
 										<div class="col-sm-6">
 											<div class="form-group">
 												<label for="pillInput">Dari tgl</label>
-												<input type="date" class="form-control input-pill" id="pillInput" placeholder="Rp">
+												<input type="date" class="form-control input-pill" id="tanggalMulai" placeholder="Rp">
 											</div>
 										</div>
 										<div class="col-sm-6">
 											<div class="form-group">
 												<label for="pillInput">Sampai tgl</label>
-												<input type="date" class="form-control input-pill" id="pillInput" placeholder="Rp">
+												<input type="date" class="form-control input-pill" id="tanggalSelesai" placeholder="Rp">
 											</div>
 										</div>
 									</div>
-									<a href="#" class="btn btn-primary btn-border btn-lg w-75 fw-bold mb-3">Proses</a>
+									<a href="#" class="btn btn-primary btn-border btn-lg w-75 fw-bold mb-3" onclick="tampilkanByDate()">Proses</a>
 								</form>
-									<div class="row py-3">
-										<div class="col-md-12">
-											<div id="chart-container">
-												<canvas id="totalIncomeChart"></canvas>
-											</div>
+								<div class="row py-3">
+									<div class="col-md-12">
+										<div id="chart-container">
+											<canvas id="totalIncomeChart"></canvas>
 										</div>
 									</div>
+								</div>
 							</div>
 						</div>
 						<div class="col-md-7">
@@ -62,80 +62,29 @@
 									</div>
 								</div>
 								<div class="card-body">
-									
-									<div class="table-responsive">
-										<table id="add-row" class="display table table-striped table-hover" >
-											<thead>
-												<tr>
-													<th>Kode</th>
-													<th>Nama</th>
-													<th>Harga</th>
-													<th style="width: 10%">Action</th>
-												</tr>
-											</thead>
-											<tbody>
-												<tr>
-													<td>001</td>
-													<td>System Architect</td>
-													<td>200.000</td>
-													<td>
-														<div class="form-button-action">
-															<button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task">
-																<i class="fa fa-edit"></i>
-															</button>
-															<button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove">
-																<i class="fa fa-times"></i>
-															</button>
+
+									<div class="table-responsive" id="tempat_tabel">
+
+									</div>
+									<br />
+									<div class="col-sm-12 col-md-12">
+										<div class="card card-stats card-round">
+											<div class="card-body ">
+												<div class="row">
+													<div class="col-5">
+														<div class="icon-big text-center">
+															<i class="flaticon-coins text-success"></i>
 														</div>
-													</td>
-												</tr>
-												<tr>
-													<td>006</td>
-													<td>Accountant</td>
-													<td>450.000</td>
-													<td>
-														<div class="form-button-action">
-															<button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task">
-																<i class="fa fa-edit"></i>
-															</button>
-															<button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove">
-																<i class="fa fa-times"></i>
-															</button>
+													</div>
+													<div class="col-7 col-stats">
+														<div class="numbers">
+															<p class="card-category">Total Keuntungan :</p>
+															<h4 class="card-title" id="keuntungan">Rp. 3.000.000</h4>
 														</div>
-													</td>
-												</tr>
-												<tr>
-													<td>007</td>
-													<td>Junior Technical Author</td>
-													<td>067</td>
-													<td>
-														<div class="form-button-action">
-															<button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task">
-																<i class="fa fa-edit"></i>
-															</button>
-															<button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove">
-																<i class="fa fa-times"></i>
-															</button>
-														</div>
-													</td>
-												</tr>
-												<tr>
-													<td>023</td>
-													<td>Software Engineer</td>
-													<td>54.000</td>
-													<td>
-														<div class="form-button-action">
-															<button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task">
-																<i class="fa fa-edit"></i>
-															</button>
-															<button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove">
-																<i class="fa fa-times"></i>
-															</button>
-														</div>
-													</td>
-												</tr>
-											</tbody>
-										</table>
+													</div>
+												</div>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -143,3 +92,61 @@
 					</div>
 				</div>
 			</div>
+			<script>
+				tampilkan()
+
+				function tampilkan() {
+					var now = new Date();
+					var day = ("0" + now.getDate()).slice(-2);
+					var month = ("0" + (now.getMonth() + 1)).slice(-2);
+					var today = now.getFullYear() + "-" + (month) + "-" + (day);
+
+					$("#tanggalMulai").val(today)
+					$("#tanggalSelesai").val(today)
+					tampilkanByDate()
+				}
+
+				function tampilkanByDate() {
+					var tanggalMulai = $("#tanggalMulai").val()
+					var tanggalSelesai = $("#tanggalSelesai").val()
+					var keuntungan = 0;
+					var totalKeuntungan = 0;
+					var tabel = '<table id="add-row" class="display table table-striped table-hover" ><thead><tr><th>NO</th><th>TANGGAL</th><th>KODE</th><th>NAMA</th><th>MERK</th><th>KULAK</th><th>JUAL</th><th>QUANTITY</th><th>TOTAL</th><th>UNTUNG</th><th>KASIR</th></tr></thead><tbody>'
+					$.ajax({
+						url: '<?= base_url() ?>keuntungan_control/get_data',
+						method: 'post',
+						data: "target=tbl_penjualan&tanggalMulai=" + tanggalMulai + "&tanggalSelesai=" + tanggalSelesai,
+						dataType: 'json',
+						success: function(data) {
+							for (let i = 0; i < data.length; i++) {
+								keuntungan = ((data[i].harga_jual - data[i].kulak) * data[i].jumlah_penjualan)
+								totalKeuntungan += keuntungan
+								tabel += '<tr>'
+								tabel += '<td>' + (i + 1) + '</td>'
+								tabel += '<td>' + formatTanggal(data[i].tgl_penjualan) + '</td>'
+								tabel += '<td>' + data[i].kode_barang + '</td>'
+								tabel += '<td>' + data[i].nama_barang + '</td>'
+								tabel += '<td>' + data[i].merk_barang + '</td>'
+								tabel += '<td>' + data[i].kulak + '</td>'
+								tabel += '<td>' + data[i].harga_jual + '</td>'
+								tabel += '<td>' + data[i].jumlah_penjualan + '</td>'
+								tabel += '<td>' + (data[i].harga_jual * data[i].jumlah_penjualan) + '</td>'
+								tabel += '<td>' + keuntungan + '</td>'
+								tabel += '<td>' + data[i].id_pengguna + '</td>'
+								tabel += '</tr>'
+							}
+							tabel += '</tbody></table>'
+							$("#tempat_tabel").html(tabel)
+							$("#keuntungan").html('Rp. ' + totalKeuntungan)
+						}
+					});
+				}
+
+				function formatTanggal(tanggal) {
+					var now = new Date(tanggal);
+					var day = ("0" + now.getDate()).slice(-2);
+					var month = ("0" + (now.getMonth() + 1)).slice(-2);
+					var today = now.getFullYear() + "-" + (month) + "-" + (day);
+					return today
+				}
+			</script>

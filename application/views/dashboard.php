ec1ee3a290f849be0aae1311e19594b19126602e
nurhayati fitriani <nurhayatifitriani77@gmail.com>
2020-09-06 20:08:35 +0700

    delete export button at dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -95,18 +95,6 @@
 									<div class="card-head-row">
 										<div class="card-title">Keuntungan</div>
 										<div class="card-tools">
-											<a href="#" class="btn btn-info btn-border btn-round btn-sm mr-2">
-												<span class="btn-label">
-													<i class="fa fa-pencil"></i>
-												</span>
-												Export
-											</a>
-											<a href="#" class="btn btn-info btn-border btn-round btn-sm">
-												<span class="btn-label">
-													<i class="fa fa-print"></i>
-												</span>
-												Print
-											</a>
 										</div>
 									</div>
 								</div>

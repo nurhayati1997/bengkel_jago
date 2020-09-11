290aa225fe706eec07a861e253f83657d7a7191b
nurhayati fitriani <nurhayatifitriani77@gmail.com>
2020-09-11 23:22:24 +0700

    logo

--- application/views/template.php
+++ application/views/template.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>Karisma Motor</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href="<?= base_url() ?>assets/img/icon.ico" type="image/x-icon" />
+	<link rel="icon" href="<?= base_url() ?>assets/img/favicon.ico" type="image/x-icon" />
 
 	<!-- Fonts and icons -->
 	<script src="<?= base_url() ?>assets/js/plugin/webfont/webfont.min.js"></script>
@@ -39,8 +39,8 @@
 			<!-- Logo Header -->
 			<div class="logo-header" data-background-color="blue">
 
-				<a href="index.html" class="logo">
-					<img src="<?= base_url() ?>assets/img/logo.svg" alt="navbar brand" class="navbar-brand">
+				<a href="<?= site_url('dashboard') ?>" class="logo">
+					<img src="<?= base_url() ?>assets/img/karisma.png" alt="navbar brand" class="navbar-brand">
 				</a>
 				<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon">

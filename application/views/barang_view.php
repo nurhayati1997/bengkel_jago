11fa833b82d3dcda7283652f90135f2a5439335d
Ainur Inas Annisa <150411100016@trunojoyo.ac.id>
2020-09-13 08:11:07 +0700

    qw

--- application/views/barang_view.php
+++ application/views/barang_view.php
@@ -470,4 +470,12 @@
 			}
 		});
 	}
+
+	function hide_allert() {
+		window.setTimeout(function() {
+			$(".alert").fadeTo(500, 0).slideUp(500, function() {
+				$(this).remove();
+			});
+		}, 2000);
+	} 
 </script>
